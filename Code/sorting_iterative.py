5084ce9d1ff110905123c072f51b985fcbbb7cf2
Jerome <jerome.schmidt@students.makeschool.com>
2020-04-02 17:12:29 -0500

    added time complexities

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -3,8 +3,8 @@
 
 def is_sorted(items):
     """Return a boolean indicating whether given items are in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    TODO: Running time: O(n) Why and under what conditions? Has to search whole list to determine if it is sorted
+    TODO: Memory usage: O(1) Why and under what conditions? No new memory created"""
     # TODO: Check that all adjacent items are in order, return early if so
     for i in range(len(items)-1):
         if items[i] > items[i+1]:
@@ -15,8 +15,8 @@ def is_sorted(items):
 def bubble_sort(items):
     """Sort given items by swapping adjacent items that are out of order, and
     repeating until all items are in sorted order.
-    TODO: Running time: O(n^2) Why and under what conditions? Worst case, visits each element n times for each element
-    TODO: Memory usage: O(n) Why and under what conditions? No new memory created, elements are swapped"""
+    TODO: Running time: O(n^2) or O(n*(n-1)) Why and under what conditions? Worst case, visits each element n times for each element
+    TODO: Memory usage: O(1) Why and under what conditions? No new memory created, elements are swapped"""
     # TODO: Repeat until all items are in sorted order
     swapped = True
     last_unsorted = len(items)-1
@@ -35,8 +35,8 @@ def bubble_sort(items):
 def selection_sort(items):
     """Sort given items by finding minimum item, swapping it with first
     unsorted item, and repeating until all items are in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    TODO: Running time: O(n^2) Why and under what conditions? Every condition
+    TODO: Memory usage: O(1) Why and under what conditions? No new memory created"""
     # TODO: Repeat until all items are in sorted order
     for i in range(len(items)):
         minimum_index = i
@@ -52,16 +52,18 @@ def selection_sort(items):
 def insertion_sort(items):
     """Sort given items by taking first unsorted item, inserting it in sorted
     order in front of items, and repeating until all items are in order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    TODO: Running time: O(n^2) Why and under what conditions? Array is in reverse of desired order
+    TODO: Memory usage: O(1) Why and under what conditions? No new memory created"""
     # TODO: Repeat until all items are in sorted order
     # TODO: Take first unsorted item
     # TODO: Insert it in sorted order in front of items
-    for i in range(1, len(items)):
-        j = i-1
-        temp = items[i]
-        while ((j >= 0) and (items[j] > temp)):
-            items[j+1] = items[j]
-            j -= 1
-        items[j+1] = temp
+    for unsorted_index in range(1, len(items)):
+        sorted_index = unsorted_index-1
+        temp = items[unsorted_index]
+
+        while ((sorted_index >= 0) and (items[sorted_index] > temp)):
+            items[sorted_index+1] = items[sorted_index]
+            sorted_index -= 1
+
+        items[sorted_index+1] = temp
     return items
