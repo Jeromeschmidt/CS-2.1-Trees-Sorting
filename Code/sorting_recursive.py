49add8cb9771f4337fb2f84a42069f8c7e1777a3
Jerome <jerome.schmidt@students.makeschool.com>
2020-04-09 17:18:38 -0500

    finished quicksort

--- Code/sorting_recursive.py
+++ Code/sorting_recursive.py
@@ -72,7 +72,7 @@ def merge_sort(items):
 
 def partition(items, low, high):
     """Return index `p` after in-place partitioning given items in range
-    `[low...high]` by choosing a pivot (TODO: document your method here) from
+    `[low...high]` by choosing a pivot (Choosing element in the middle) from
     that range, moving pivot into index `p`, items less than pivot into range
     `[low...p-1]`, and items greater than pivot into range `[p+1...high]`.
     TODO: Running time: ??? Why and under what conditions?
@@ -82,6 +82,13 @@ def partition(items, low, high):
     # TODO: Move items less than pivot into front of range [low...p-1]
     # TODO: Move items greater than pivot into back of range [p+1...high]
     # TODO: Move pivot item into final position [p] and return index p
+    pivot_index = low
+    for i in range(low, high+1):
+        if items[i] <= items[pivot_index]:
+            items[low], items[i] = items[i], items[low]
+            i += 1
+    return pivot_index
+
 
 
 def quick_sort(items, low=None, high=None):
@@ -94,7 +101,20 @@ def quick_sort(items, low=None, high=None):
     # TODO: Check if list or range is so small it's already sorted (base case)
     # TODO: Partition items in-place around a pivot and get index of pivot
     # TODO: Sort each sublist range by recursively calling quick sort
+    if high is None:
+        high = len(items)-1
+    if low is None:
+        low = 0
+
+    if low < high:
+
+        pt = partition(items, low, high)
+
+        quick_sort(items, low, pt-1)
+        quick_sort(items, pt+1, high)
+    return items
 
 # merge_sort([1,3,5,2,4,6,3])
 # print(merge_sort([1,3,5,2,4,6,3]))
-print(merge_sort([1,3,5,2,4,6,3]))
+print(quick_sort([1,3,5,2,4,6,3]))
+# print(quick_sort([5,7,1,3,4,6,3]))
