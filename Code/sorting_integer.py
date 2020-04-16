59fba812e51ae49ee29b96804f701a2d5c742c8e
Jerome <jerome.schmidt@students.makeschool.com>
2020-04-16 17:27:34 -0500

    update to bucket sort

--- Code/sorting_integer.py
+++ Code/sorting_integer.py
@@ -1,5 +1,5 @@
 #!python
-
+from sorting_iterative import insertion_sort
 
 def counting_sort(numbers):
     """Sort given numbers (integers) by counting occurrences of each number,
@@ -38,15 +38,33 @@ def bucket_sort(numbers, num_buckets=10):
     # TODO: Sort each bucket using any sorting algorithm (recursive or another)
     # TODO: Loop over buckets and append each bucket's numbers into output list
     # FIXME: Improve this to mutate input instead of creating new output list
-    range = (max(numbers) - min(numbers))/num_buckets
+    if numbers == []:
+        return []
+
+    bucket_range = (max(numbers) - min(numbers))/num_buckets
     ranges = list()
-    print(type(num_buckets))
 
     for i in range(num_buckets):
-        ranges.append(((min(numbers)+(r*range)),((min(numbers)+(r*range))+range)))
-    return ranges
+        ranges.append([(min(numbers)+(i*bucket_range)-0.00001),((min(numbers)+(i*bucket_range)-0.00001)+bucket_range)])
 
-# print(counting_sort([1,9,3,3,5,3,7,8,9]))
+    ranges[len(ranges)-1][1] += 1
+    buckets = list()
+
+    for i in range(len(ranges)):
+        temp = list()
+        for j in range(len(numbers)):
+            if((numbers[j] >= ranges[i][0]) and (numbers[j] < ranges[i][1])):
+                temp.append(numbers[j])
+        buckets.append(temp)
+
+    result = list()
+    for bucket in buckets:
+        result += insertion_sort(bucket)
+
+    return result
+
+
+print(counting_sort([1,9,3,3,5,3,7,8,9]))
 # print(int("5", 36))
 # print(counting_sort(["b","a","d","c"]))
 # print(ord(chr(int("abc", 36))))
